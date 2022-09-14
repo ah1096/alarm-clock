e8739674fc01554023604fcbe69cba8fb735ecb9
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-09-14 17:31:53 +0000

    created time variables and currentTime function

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert("hello!");
+//add zeroes when displaying numbers less than 10
+function addZero(i) {
+    if (i < 10) {i = "0" + i}
+    return i;
+}
+
+//declare variables
+const date = new Date();
+let hours = addZero(date.getHours());
+let minutes = addZero(date.getHours());
+let seconds = addZero(date.getSeconds());
+
+//show the current time in 00:00:00 format
+let currentTime = hours + ":" + minutes + ":" + seconds;
