06e529174d53cf1c2ee43e990ad3266ce0a2b880
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-09-14 19:45:19 +0000

    created variables and for loop for updateClock

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 function updateClock(){
+    var now = new Date();
+    var dayname = now.getDay(),
+        month = now.getMonth(),
+        date = now.getDate(),
+        year = now.getFullYear,
+        hr = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        period = "AM";
 
-}
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var ids = ["dayname", "month", "daynumber", "year", "hour", "minutes", "seconds", "period"];
+        var values = [weekdays[dayname], months[month], date, year, hr, min, sec, period];
+
+        for(var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values [i];
+    }
 
 function initClock() {
     updateClock();
